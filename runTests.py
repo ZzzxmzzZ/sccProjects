c06b5da917c107f0f9164e8512bdce45282fa2ef
ZzzxmzzZ <873802061@qq.com>
2022-04-07 18:22:51 +0800

    测试报告

--- runTests.py
+++ runTests.py
@@ -2,20 +2,21 @@
 import time
 import unittest
 
+from common.BeautifulReport import BeautifulReport
 from common.log import Logger
 from testCase.oldtestLogin import TestLogin
 
 log = Logger(__name__)
 def testFirst():
     now = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime(time.time()))
-    # reportTitle = "作业管理系统功能测试报告" + now + ".html"
-    # desc = "登录、个人中心测试用例"
+    reportTitle = "作业管理系统功能测试报告" + now + ".html"
+    desc = "登录测试用例"
 
     suite = unittest.TestSuite()
     lorder1 = unittest.TestLoader().loadTestsFromTestCase(TestLogin)
     suite.addTest(lorder1)
-    # runner = BeautifulReport(suite)
-    # runner.report(filename=reportTitle, description=desc, report_dir=reportPath)
+    runner = BeautifulReport(suite)
+    runner.report(filename=reportTitle, description=desc, report_dir=reportPath)
 
 if __name__=="__main__":
     #执行各功能点用例
